abcbbbffe73baed74f769d2c546ee0c66cede5aa
vishakha <warevishakha1@gmail.com>
2022-12-23 21:14:10 +0530

    ok

--- src/app/default-layout/category/category.page.ts
+++ src/app/default-layout/category/category.page.ts
@@ -8,27 +8,47 @@ import { StorageService, UtilitiesService } from 'src/app/core/services';
   styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
-  // loaded = false;
-  // categoryDetails: any = {};
-  // service: any;
+  loaded:boolean= false;
+   categoryDetails: any = [];
+   user:any;
+   businessTypeId:any;
+   categoryTypeId:string;
+   selectedCategoryId:string;
+   selectedBusinessName:string
   constructor(
     private router:Router,
-    private customer:CustomerService
+    private actRoute:ActivatedRoute,
+    private categoryService:CategoryService ,
+    private localStorage: StorageService
     ) { }
 
-  ngOnInit() {
-  }
-  
-  // category(){
-  //   this.loaded = false;
-  //   this.customer.getCategory(this.service._id).subscribe((success) => {
-  //     console.log("success",success);
-  //     this.categoryDetails = success;
-      
-      
-  //     // this.spinner.hideLoader();
-  //     this.loaded = false;
-  //   });
-  // }
+
+businessName:string;
+ngOnInit() {
+    
+  this.user = this.localStorage.get('OBUser');
+  this.actRoute.queryParams.subscribe(success=>{
+console.log("success",success);
+this.businessName = success.name;
+this. getByBusinessTypeCategory(success._id)
+
+  })
+}
+// getByIdCatlogue(){
+
+// }
+
+getByBusinessTypeCategory(businessTypeId){
+let obj :any= {businessTypeId:businessTypeId}
+this.categoryService.getAll(obj).subscribe((success) => {
+  console.log("success-----------", success);
+  this.categoryDetails=success.rows;
+  this.selectedCategoryId = success.rows[0]._id;
+      this.selectedCategoryId = success.rows[0].name;
+      console.log("this.selectedCategoryId",this.selectedCategoryId);
+    // (this.selectedCategoryId,this.selectedBusinessName);
+        
+});
+}
 
 }
