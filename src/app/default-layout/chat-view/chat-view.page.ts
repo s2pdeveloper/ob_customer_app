2ad82defb1f19a36a195bc65e44fe5e790e4ec47
Paritosh Bangadkar <paritoshbangadkar@gmail.com>
2023-01-10 14:36:45 +0530

    chat design done

--- src/app/default-layout/chat-view/chat-view.page.ts
+++ src/app/default-layout/chat-view/chat-view.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { ChatService } from 'src/app/service/chat/chat.service';
 import { StorageService, ToastService } from 'src/app/core/services';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat-view',
@@ -12,12 +13,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./chat-view.page.scss'],
 })
 export class ChatViewPage implements OnInit {
+  @ViewChild(IonInfiniteScroll, { static: false })
+  infiniteScroll: IonInfiniteScroll;
+  disabledScroll = false;
+  page: number = 1;
+  pageSize: number = 10;
+  search = '';
+
   shopId: any;
   msgArr: any = [];
   user: any = {};
+  msg: any;
+  customerId: any;
+  message: string;
+ 
+  
+  
 
   constructor(
-
     private activatedRoute: ActivatedRoute,
     public translate: TranslateService,
     private chatService: ChatService,
@@ -38,11 +51,13 @@ export class ChatViewPage implements OnInit {
   ngOnInit() {
     this.user = this.localStorage.get('OBUser');
     this.activatedRoute.queryParams.subscribe((params) => {
+      console.log("params------",params);
+      
       if (params.shopId) {
         this.shopId = params.shopId;
       }
     });
-    this.getMsgByCustomerId();
+    this.getMsgByCustomerId(false);
   }
 
   sendMessage() {
@@ -50,7 +65,7 @@ export class ChatViewPage implements OnInit {
     this.chatService.create(this.chatForm.value).subscribe(
       (success) => {
         console.log('success', success);
-        this.getMsgByCustomerId();
+        this.getMsgByCustomerId(false);
         this.chatForm.reset();
         this.spinner.hideLoader();
       },
@@ -61,14 +76,23 @@ export class ChatViewPage implements OnInit {
     );
   }
 
-  getMsgByCustomerId() {
+  getMsgByCustomerId(isFirstLoad: boolean, event?: any) {
     this.spinner.showLoader();
     this.chatService
       .getMsgByCustomerId(this.user._id)
       .subscribe((success: any) => {
-        console.log('getMsgByCustomerId-------', success);
+        console.log('getMsgByCustomer----', success);
         this.msgArr = success.payload.rows;
         this.spinner.hideLoader();
       });
+  }
+
+  doInfinite(event) {
+    console.log('In do');
+    this.page++;
+    this.getMsgByCustomerId(true, event);
+    event.target.disabled = true;
+    this.infiniteScroll.disabled = true;
+    event.target.complete();
   }
 }
