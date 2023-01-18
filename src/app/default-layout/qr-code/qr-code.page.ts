68f6254ee2d31c775f5e87aecc8f5023817ffe42
pratikme14123 <pbangadkar1997@gmail.com>
2023-01-18 13:35:16 +0530

    QR code

--- src/app/default-layout/qr-code/qr-code.page.ts
+++ src/app/default-layout/qr-code/qr-code.page.ts
@@ -8,17 +8,28 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 export class QrCodePage implements OnInit {
   scanActive: boolean = false;
   data: any;
+  encodedData: any;
+  url: any = 'http://localhost:8100/shop-detail?_id=';
 
   constructor(private barcodeScanner: BarcodeScanner) {}
 
   ngOnInit() {
-    this.scan();
+    // this.scan();
   }
 
   scan() {
     this.data = null;
     this.barcodeScanner
-      .scan()
+      .scan({
+        preferFrontCamera: false,
+        showFlipCameraButton: true,
+        showTorchButton: true,
+        torchOn: false,
+        // prompt: 'Place a barcode inside the scan area',
+        resultDisplayDuration: 500,
+        formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+        orientation: 'portrait',
+      })
       .then((barcodeData) => {
         console.log('Barcode data', barcodeData);
         this.data = barcodeData;
@@ -26,5 +37,19 @@ export class QrCodePage implements OnInit {
       .catch((err) => {
         console.log('Error', err);
       });
+  }
+
+  createBarcode() {
+    this.barcodeScanner
+      .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.url)
+      .then(
+        (encodedData) => {
+          console.log(encodedData);
+          this.encodedData = encodedData;
+        },
+        (err) => {
+          console.log('Error occured : ' + err);
+        }
+      );
   }
 }
