a2ae1be60f8181927fce59c0b4c73c19b6c95a9e
haresh_gahane <hareshgahane4u@gmail.com>
2023-01-30 15:32:51 +0530

    geolocation issue resolve

--- src/app/default-layout/map/map.page.ts
+++ src/app/default-layout/map/map.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  ViewChild, ElementRef, NgZone } from '@angular/core';
+import { ViewChild, ElementRef, NgZone } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 
@@ -11,125 +11,152 @@ declare var google;
 })
 export class MapPage implements OnInit {
 
-  @ViewChild('map',  {static: false}) mapElement: ElementRef;
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
   map: any;
-  address:string;
+  address: string;
   lat: string;
-  long: string;  
+  long: string;
   autocomplete: { input: string; };
   autocompleteItems: any[];
   location: any;
   placeid: any;
   GoogleAutocomplete: any;
+  geocoder: any;
+  markers: any[];
   constructor(
     private geolocation: Geolocation,
-    private nativeGeocoder: NativeGeocoder,    
+    private nativeGeocoder: NativeGeocoder,
     public zone: NgZone,
-  ) { 
+  ) {
     this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
     this.autocomplete = { input: '' };
     this.autocompleteItems = [];
   }
 
   ngOnInit() {
-    this.loadMap(); 
+    this.loadMap();
   }
 
   loadMap() {
-    
     //FIRST GET THE LOCATION FROM THE DEVICE.
-    this.geolocation.getCurrentPosition().then((resp) => {
-      console.log("resp,resp",resp);
-      
-      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      } 
-      console.log("V@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",latLng);
-      
-      //LOAD THE MAP WITH THE PREVIOUS VALUES AS PARAMETERS.
-      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude); 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions); 
-      this.map.addListener('tilesloaded', () => {
-        console.log('accuracy',this.map, this.map.center.lat());
-        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
-        this.lat = this.map.center.lat()
-        this.long = this.map.center.lng()
-      }); 
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
+    if (navigator.geolocation) {
+      this.geolocation.getCurrentPosition().then((resp) => {
+        console.log("resp,resp", resp);
+
+        let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        console.log("V@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", latLng);
+
+        //LOAD THE MAP WITH THE PREVIOUS VALUES AS PARAMETERS.
+        this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        this.map.addListener('tilesloaded', () => {
+          console.log('accuracy', this.map, this.map.center.lat());
+          this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+          this.lat = this.map.center.lat()
+          this.long = this.map.center.lng()
+        });
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    }
   }
 
-  
+
   getAddressFromCoords(lattitude, longitude) {
-    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    console.log("getAddressFromCoords " + lattitude + " " + longitude);
     let options: NativeGeocoderOptions = {
       useLocale: true,
-      maxResults: 5    
-    }; 
+      maxResults: 5
+    };
     this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
       .then((result: NativeGeocoderResult[]) => {
         this.address = "";
         let responseAddress = [];
         for (let [key, value] of Object.entries(result[0])) {
-          if(value.length>0)
-          responseAddress.push(value); 
+          if (value.length > 0)
+            responseAddress.push(value);
         }
         responseAddress.reverse();
         for (let value of responseAddress) {
-          this.address += value+", ";
+          this.address += value + ", ";
         }
         this.address = this.address.slice(0, -2);
       })
-      .catch((error: any) =>{ 
+      .catch((error: any) => {
         this.address = "Address Not Available!";
-      }); 
+      });
   }
 
   //FUNCTION SHOWING THE COORDINATES OF THE POINT AT THE CENTER OF THE MAP
-  ShowCords(){
-    alert('lat' +this.lat+', long'+this.long )
+  ShowCords() {
+
+    alert('lat' + this.lat + ', long' + this.long)
   }
-  
+
   //AUTOCOMPLETE, SIMPLY LOAD THE PLACE USING GOOGLE PREDICTIONS AND RETURNING THE ARRAY.
-  UpdateSearchResults(){
+  UpdateSearchResults() {
     if (this.autocomplete.input == '') {
       this.autocompleteItems = [];
       return;
     }
     this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
-    (predictions, status) => {
-      this.autocompleteItems = [];
-      this.zone.run(() => {
-        predictions.forEach((prediction) => {
-          this.autocompleteItems.push(prediction);
+      (predictions, status) => {
+        this.autocompleteItems = [];
+        this.zone.run(() => {
+          predictions.forEach((prediction) => {
+            this.autocompleteItems.push(prediction);
+          });
         });
       });
-    });
   }
-  
+
   //wE CALL THIS FROM EACH ITEM.
   SelectSearchResult(item) {
+    console.log(item);
+
     ///WE CAN CONFIGURE MORE COMPLEX FUNCTIONS SUCH AS UPLOAD DATA TO FIRESTORE OR LINK IT TO SOMETHING
-    alert(JSON.stringify(item))      
-    this.placeid = item.place_id
+    // alert(JSON.stringify(item))      
+    // this.placeid = item.place_id
+    // this.GoTo()
+      // this.clearMarkers();
+      this.autocompleteItems = [];
+      this.geocoder = new google.maps.Geocoder;
+      this.markers = [];
+
+      this.geocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
+        if (status === 'OK' && results[0]) {
+          let position = {
+            lat: results[0].geometry.location.lat,
+            lng: results[0].geometry.location.lng
+          };
+          let marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: this.map,
+          });
+          this.markers.push(marker);
+          this.map.setCenter(results[0].geometry.location);
+        }
+      })
+    
   }
-  
-  
+
+
   //lET'S BE CLEAN! THIS WILL JUST CLEAN THE LIST WHEN WE CLOSE THE SEARCH BAR.
-  ClearAutocomplete(){
+  ClearAutocomplete() {
     this.autocompleteItems = []
     this.autocomplete.input = ''
   }
- 
+
   //sIMPLE EXAMPLE TO OPEN AN URL WITH THE PLACEID AS PARAMETER.
-  GoTo(){
-    return window.location.href = 'https://www.google.com/maps/search/?api=1&query=Google&query_place_id='+this.placeid;
+  GoTo() {
+    return window.location.href = 'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=' + this.placeid;
   }
-  
+
 }
 
 
